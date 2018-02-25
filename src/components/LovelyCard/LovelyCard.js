a86b3f98dd3b351096886f350c3966d348892320
Sara Javed <sara.joann.javed@gmail.com>
2018-02-25 03:55:46 -0500

    adding information to the Interests tab of the LovelyCard.

--- src/components/LovelyCard/LovelyCard.js
+++ src/components/LovelyCard/LovelyCard.js
@@ -29,14 +29,36 @@ const LovelyCard = () => (
 
       	<div className="row">
       		<div className="col s4 center">
-      			<i class="material-icons" style={style.lovelyIcons}>code</i>
+      			<div style={style.lovelyBox}>
+	      			<i class="material-icons" style={style.lovelyIcons}>local_play</i>
+	      			<h5>FullStack Web Development</h5>
+	      			<p>Certification, Rutgers '18</p>
+	      		</div>
+      			<p style={style.justify}>
+      			To push myself personally and professionally, I completed a 6-month intensive certification program focusing on: JavaScript, jQuery, Node.js, Express, HTML5, CSS3, Bootstrap, Handlebars, React JS, API/JSON, MySQL, Sequelize, Firebase, MongoDB, Mongoose.
+      			</p>
       		</div>
+
       		<div className="col s4 center">
-      			bb
+      			<div style={style.lovelyBox}>
+	      			<i class="material-icons" style={style.lovelyIcons}>local_library</i>
+	      			<h5>New York Stories</h5>
+	      		</div>
+      			<p style={style.justify}>
+      			Putting roots down in New York City has opened up an interest in stories about its people and places. Michael Cunningham is one of my favorite authors. 
+      			</p>
       		</div>
+
       		<div className="col s4 center">
-      			cc
+	      		<div style={style.lovelyBox}>
+	      			<i class="material-icons" style={style.lovelyIcons}>mood_bad</i>
+	      			<h5>Catfish</h5>
+      			</div>
+      			<p style={style.justify}>
+      			No one mentions this show casually - you either love it or hate it with every atom of your being. Does the show hinge on nothing more than a basic knowledge of Google Images and Facebook? Yes. Have I bought episodes on Amazon? Absolutely. 
+      			</p>
       		</div>
+
       	</div>
 
       </div>
@@ -73,7 +95,7 @@ const LovelyCard = () => (
 	      			<p>BA, Tufts University '14</p>
       			</div>
       			<p style={style.justify}>
-      			I am a medievalist at heart and studied Islamic portable objects and manuscripts. Many pieces served as the AR of their times, using imagery or space to inform the user's overall experience or interpretation of their surroundings. 
+      			I am a medievalist at heart and studied Islamic portable objects and manuscripts. Many pieces could be considered early iterations of AR as they destabilize the eye and break the bonds of their own materiality. Shout out to muqarnas for making the finite seem infinite!
       			</p>
       		</div>
 
@@ -85,14 +107,37 @@ const LovelyCard = () => (
 
       	<div className="row">
       		<div className="col s4 center">
-      			<i class="material-icons" style={style.lovelyIcons}>code</i>
+      			<div style={style.lovelyBox}>
+	      			<i class="material-icons" style={style.lovelyIcons}>local_florist</i>
+	      			<h5>Cultivating Relationships</h5>
+	      		</div>
+      			<p style={style.justify}>
+      			To push myself personally and professionally, I completed a 6-month intensive certification program focusing on: JavaScript, jQuery, Node.js, Express, HTML5, CSS3, Bootstrap, Handlebars, React JS, API/JSON, MySQL, Sequelize, Firebase, MongoDB, Mongoose.
+      			</p>
       		</div>
+
       		<div className="col s4 center">
-      			bb
+      			<div style={style.lovelyBox}>
+	      			<i class="material-icons" style={style.lovelyIcons}>extension</i>
+	      			<h5>Problem Solving</h5>
+	      			<p>BA, Tufts University '14</p>
+	      		</div>
+      			<p style={style.justify}>
+      			The study of life span development has always fascinated me. My coursework focused on inequities in U.S. education through the lens of language acquisition, bilingual education, and public policy.
+      			</p>
       		</div>
+
       		<div className="col s4 center">
-      			cc
+	      		<div style={style.lovelyBox}>
+	      			<i class="material-icons" style={style.lovelyIcons}>palette</i>
+	      			<h5>Art History</h5>
+	      			<p>BA, Tufts University '14</p>
+      			</div>
+      			<p style={style.justify}>
+      			I am a medievalist at heart and studied Islamic portable objects and manuscripts. Many pieces could be considered early iterations of AR as they destabilize the eye and break the bonds of their own materiality. Shout out to muqarnas for making the finite seem infinite!
+      			</p>
       		</div>
+
       	</div>
 
       </div>
