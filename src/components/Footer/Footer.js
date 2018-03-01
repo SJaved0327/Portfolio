a4110d0c0ec3b74088194931eb0333c4f8c961a8
Sara Javed <sara.joann.javed@gmail.com>
2018-02-28 23:42:15 -0500

    finished social media links in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,22 +11,30 @@ const Footer = () => (
 	  <div className="container">
 	    <div className="row">
 	      <div className="col l6 s12">
-	        <h5 className="grey-text text-darken-2">Footer Content</h5>
-	        <p className="grey-text text-darken-2">You can use rows and columns here to organize your footer content.</p>
+	      	<div className="row">
+	      		<div className="col l12 s12 center">
+	      			<img src="../../../images/gray_brand_logo.png" alt="logo of sara javed name" height="75px" />
+	      		</div>
+	      	</div>
+	        <p className="grey-text text-darken-2">Thanks for visiting!</p>
 	      </div>
 	      <div className="col l4 offset-l2 s12">
-	        <h5 className="red-text text-lighten-1">Contact</h5>
+	        <h5 className="grey-text text-darken-2">Contact</h5>
 	        <ul>
-	          <li><a href="https://github.com/SJaved0327" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-github-alt"></i></span></div></a></li>
-		        <li><a href="https://www.linkedin.com/in/sarajaved/" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-linkedin-in"></i></span></div></a></li>
-		        <li><a href="https://www.instagram.com/heyheysarajay/" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-instagram"></i></span></div></a></li>
-		        <li><a href="https://twitter.com/heyheysarajay" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-twitter"></i></span></div></a></li>
+	        	<li><a href="mailto:sara.joann.javed@gmail.com" target="_blank"><span style={style.icons}><i class="far fa-envelope"></i></span> sara.joann.javed@gmail.com</a></li>
+	          <li><a href="https://github.com/SJaved0327" target="_blank"><span style={style.icons}><i class="fab fa-github-alt"></i></span> github.com/SJaved0327</a></li>
+		        <li><a href="https://www.linkedin.com/in/sarajaved/" target="_blank"><span style={style.icons}><i class="fab fa-linkedin-in"></i></span> linkedin.com/in/sarajaved/</a></li>
+		        <li><a href="https://www.instagram.com/heyheysarajay/" target="_blank"><span style={style.icons}><i class="fab fa-instagram"></i></span> instagram.com/heyheysarajay/</a></li>
+		        <li><a href="https://twitter.com/heyheysarajay" target="_blank"><span style={style.icons}><i class="fab fa-twitter"></i></span> twitter.com/heyheysarajay</a></li>
 	        </ul>
 	      </div>
 	    </div>
 	  </div>
-	  <div className="footer-copyright">
-	  </div>
+	  <div class="footer-copyright">
+      <div class="container grey-text text-darken-2">
+      © 2018
+      </div>
+    </div>
 	</footer>
 )
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ const style = {
     marginTop: '10px'
 	},
   icons: {
-    fontSize: '2em'
+    fontSize: '1.5em'
   },
   logo: {
     width: '200px',
@@ -20,6 +20,7 @@ const Nav = () => (
     <div style={style.margin}>
       <a href="/" className="brand-logo" style={style.logo}><img src="../../../images/gray_brand_logo.png" alt="logo of sara javed name" height="75px" /></a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="mailto:sara.joann.javed@gmail.com" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="far fa-envelope"></i></span></div></a></li>
         <li><a href="https://github.com/SJaved0327" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-github-alt"></i></span></div></a></li>
         <li><a href="https://www.linkedin.com/in/sarajaved/" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-linkedin-in"></i></span></div></a></li>
         <li><a href="https://www.instagram.com/heyheysarajay/" target="_blank"><div style={style.icons}><span className="grey-text text-darken-2"><i class="fab fa-instagram"></i></span></div></a></li>
